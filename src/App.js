d1b4410155c4bafd58d0265609d455882974e3cb
npapenkov <npapenkov@emphasoft.com>
2021-08-17 12:33:17 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import classes from './App.module.css';
 import React from 'react';
-
 import LoginField from './components/LoginField/LoginField.js'
 import UserList from './components/UserList/UserList.js'
-// import Modal from './components/Modal/Modal.js'
+
+import { useEffect, useRef } from 'react';
 
 import reducer from './reducer';
 
@@ -29,6 +29,24 @@ function App() {
     });
     console.log('some1 has joined');
   }
+
+  function dataOnauth(data) {console.log(data)}
+
+  // useEffect(() => {
+  //   window.TelegramLoginWidget = {
+  //     callbackOnAuth: user => dataOnauth(user)
+  //   };
+  //   const script = document.createElement('script');
+  //   script.src = "https://telegram.org/js/telegram-widget.js?15";
+  //   script.setAttribute('data-telegram-login', "Lin444bot");
+  //   script.setAttribute('data-size', 'large');
+  //   script.setAttribute('data-request-access', 'write');
+  //   script.setAttribute('data-userpic', true);
+  //   script.setAttribute('data-onauth', 'TelegramLoginWidget.callbackOnAuth(user)');
+  //   script.async = true;
+  //   document.body.appendChild(script);
+  //   // this.instance.appendChild(script);
+  // });
 
   return (
     <Router>
