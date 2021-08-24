d1b54eb08503c3b86f2d5e3fd3fbbed1f181453b
npapenkov <npapenkov@emphasoft.com>
2021-08-24 20:19:56 +0300

    a

--- src/utils/getAuthToken.js
+++ src/utils/getAuthToken.js
@@ -2,14 +2,16 @@ import axios from 'axios'
 
 export const saveAuthToken =  async (a) => {
 
-  // // const loginData = {
-  // //   "username": "test_super",
-  // //   "password": "Nf<U4f<rDbtDxAPn"
-  // // }
-  //
-  // const result = await axios.post('http://emphasoft-test-assignment.herokuapp.com/api-token-auth/', loginData)
-  // window.localStorage.setItem('token', result.data.token);
-  // console.log(result.data.token, '<- token; also got getItem ->', window.localStorage.getItem('token'))
+  const loginData = {
+    "username": "test_super",
+    "password": "Nf<U4f<rDbtDxAPn"
+  }
+
+  console.log('LOOOOL?')
+
+  const result = await axios.post('http://emphasoft-test-assignment.herokuapp.com/api-token-auth/', loginData)
+  window.localStorage.setItem('token', result.data.token);
+  console.log(result.data.token, '<- token; also got getItem ->', window.localStorage.getItem('token'))
 };
 
 export const getAuthToken = () => {
